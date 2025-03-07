7d48b1ea514f2dee57308d0b54e6073cf2d527d4
Alganaut <emretug624@gmail.com>
2025-03-07 08:07:04 +0000

    Advancement tweaks

--- src/main/java/com/alganaut/hominid/registry/datagen/ModDatapackProvider.java
+++ src/main/java/com/alganaut/hominid/registry/datagen/ModDatapackProvider.java
@@ -1,10 +1,9 @@
 package com.alganaut.hominid.registry.datagen;
 
 import com.alganaut.hominid.Hominid;
-import com.alganaut.hominid.registry.world.ModBiomeModifiers;
+import com.alganaut.hominid.registry.world.HominidBiomeModifiers;
 import net.minecraft.core.HolderLookup;
 import net.minecraft.core.RegistrySetBuilder;
-import net.minecraft.core.registries.Registries;
 import net.minecraft.data.PackOutput;
 import net.neoforged.neoforge.common.data.DatapackBuiltinEntriesProvider;
 import net.neoforged.neoforge.registries.NeoForgeRegistries;
@@ -14,7 +13,7 @@ import java.util.concurrent.CompletableFuture;
 
 public class ModDatapackProvider extends DatapackBuiltinEntriesProvider {
     public static final RegistrySetBuilder BUILDER = new RegistrySetBuilder()
-            .add(NeoForgeRegistries.Keys.BIOME_MODIFIERS, ModBiomeModifiers::bootstrap);
+            .add(NeoForgeRegistries.Keys.BIOME_MODIFIERS, HominidBiomeModifiers::bootstrap);
 
     public ModDatapackProvider(PackOutput output, CompletableFuture<HolderLookup.Provider> registries) {
         super(output, registries, BUILDER, Set.of(Hominid.MODID));
--- src/main/java/com/alganaut/hominid/registry/world/HominidBiomeModifiers.java
+++ src/main/java/com/alganaut/hominid/registry/world/HominidBiomeModifiers.java
@@ -1,23 +1,21 @@
 package com.alganaut.hominid.registry.world;
 
 import com.alganaut.hominid.Hominid;
-import com.alganaut.hominid.registry.entity.ModEntityCreator;
+import com.alganaut.hominid.registry.entity.HominidEntityCreator;
 import net.minecraft.core.HolderSet;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.data.worldgen.BootstrapContext;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.resources.ResourceLocation;
-import net.minecraft.tags.BiomeTags;
 import net.minecraft.world.level.biome.Biomes;
 import net.minecraft.world.level.biome.MobSpawnSettings;
-import net.minecraft.world.level.levelgen.GenerationStep;
 import net.neoforged.neoforge.common.world.BiomeModifier;
 import net.neoforged.neoforge.common.world.BiomeModifiers;
 import net.neoforged.neoforge.registries.NeoForgeRegistries;
 
 import java.util.List;
 
-public class ModBiomeModifiers {
+public class HominidBiomeModifiers {
     public static final ResourceKey<BiomeModifier> SPAWN_MELLIFIED = registerKey("spawn_mellified");
     public static final ResourceKey<BiomeModifier> SPAWN_INCENDIARY = registerKey("spawn_incendiary");
 
@@ -25,10 +23,10 @@ public class ModBiomeModifiers {
         var biomes = context.lookup(Registries.BIOME);
         context.register(SPAWN_MELLIFIED, new BiomeModifiers.AddSpawnsBiomeModifier(
                 HolderSet.direct(biomes.getOrThrow(Biomes.FLOWER_FOREST), biomes.getOrThrow(Biomes.MEADOW) , biomes.getOrThrow(Biomes.CHERRY_GROVE)),
-                List.of(new MobSpawnSettings.SpawnerData(ModEntityCreator.MELLIFIED.get(), 100, 2, 3))));
+                List.of(new MobSpawnSettings.SpawnerData(HominidEntityCreator.MELLIFIED.get(), 100, 2, 3))));
         context.register(SPAWN_INCENDIARY, new BiomeModifiers.AddSpawnsBiomeModifier(
                 HolderSet.direct(biomes.getOrThrow(Biomes.TAIGA), biomes.getOrThrow(Biomes.SNOWY_TAIGA), biomes.getOrThrow(Biomes.OLD_GROWTH_SPRUCE_TAIGA), biomes.getOrThrow(Biomes.OLD_GROWTH_PINE_TAIGA), biomes.getOrThrow(Biomes.WINDSWEPT_HILLS) , biomes.getOrThrow(Biomes.GROVE)),
-                List.of(new MobSpawnSettings.SpawnerData(ModEntityCreator.INCENDIARY.get(), 100, 1, 3))));
+                List.of(new MobSpawnSettings.SpawnerData(HominidEntityCreator.INCENDIARY.get(), 100, 1, 3))));
     }
 
     private static ResourceKey<BiomeModifier> registerKey(String name) {
