4994029d171acb207fcfef2069f9ec0ad7501b2c
ItsHamza <99992411+hamzaalshoky@users.noreply.github.com>
2025-06-16 12:12:10 +0300

    stuff

--- src/main/java/com/alganaut/hominid/registry/entity/client/FamishedModel.java
+++ src/main/java/com/alganaut/hominid/registry/entity/client/FamishedModel.java
@@ -14,15 +14,12 @@ import net.minecraft.world.entity.ai.attributes.Attributes;
 public class FamishedModel<T extends Famished> extends HierarchicalModel<T> {
 
     private final ModelPart famished;
+    private final ModelPart torso;
     private final ModelPart head;
-    private final ModelPart body;
-    private final ModelPart head_hungry;
-
     public FamishedModel(ModelPart root) {
         this.famished = root.getChild("famished");
-        this.head = this.famished.getChild("head");
-        this.head_hungry = this.famished.getChild("head_hungry");
-        this.body = this.famished.getChild("body");
+        this.torso = this.famished.getChild("torso");
+        this.head = this.torso.getChild("head");
     }
 
     public static LayerDefinition createBodyLayer() {
@@ -31,21 +28,20 @@ public class FamishedModel<T extends Famished> extends HierarchicalModel<T> {
 
         PartDefinition famished = partdefinition.addOrReplaceChild("famished", CubeListBuilder.create(), PartPose.offset(0.0F, 24.0F, 0.0F));
 
-        PartDefinition head = famished.addOrReplaceChild("head", CubeListBuilder.create().texOffs(0, 0).addBox(-4.0F, -8.0F, -4.0F, 8.0F, 8.0F, 8.0F, new CubeDeformation(0.0F)), PartPose.offset(0.0F, -24.0F, 0.0F));
+        PartDefinition torso = famished.addOrReplaceChild("torso", CubeListBuilder.create().texOffs(0, 16).addBox(-4.0F, -10.0F, -2.0F, 8.0F, 8.0F, 4.0F, new CubeDeformation(0.0F)), PartPose.offset(0.0F, -14.0F, 0.0F));
 
-        PartDefinition head_hungry = famished.addOrReplaceChild("head_hungry", CubeListBuilder.create().texOffs(28, 22).addBox(-4.0F, -8.0F, -4.0F, 8.0F, 8.0F, 8.0F, new CubeDeformation(0.0F))
-                .texOffs(38, 38).addBox(-2.0F, 0.0F, -4.0F, 4.0F, 2.0F, 0.0F, new CubeDeformation(0.0F)), PartPose.offset(0.0F, -24.0F, 0.0F));
+        PartDefinition tummy = torso.addOrReplaceChild("tummy", CubeListBuilder.create().texOffs(24, 16).addBox(-3.0F, -1.0F, -2.0F, 6.0F, 2.0F, 4.0F, new CubeDeformation(0.0F)), PartPose.offset(0.0F, -1.0F, 0.0F));
 
-        PartDefinition body = famished.addOrReplaceChild("body", CubeListBuilder.create().texOffs(0, 16).addBox(-4.0F, -9.5F, -2.0F, 8.0F, 8.0F, 4.0F, new CubeDeformation(0.0F))
-                .texOffs(24, 16).addBox(-3.0F, -1.5F, -2.0F, 6.0F, 2.0F, 4.0F, new CubeDeformation(0.0F)), PartPose.offset(0.0F, -14.5F, 0.0F));
+        PartDefinition head = torso.addOrReplaceChild("head", CubeListBuilder.create().texOffs(28, 22).addBox(-4.0F, -8.0F, -4.0F, 8.0F, 8.0F, 8.0F, new CubeDeformation(0.0F))
+                .texOffs(37, 38).addBox(-3.0F, 0.0F, -4.0F, 6.0F, 2.0F, 0.0F, new CubeDeformation(0.0F)), PartPose.offset(0.0F, -10.0F, 0.0F));
 
-        PartDefinition left_arm = famished.addOrReplaceChild("left_arm", CubeListBuilder.create().texOffs(14, 46).mirror().addBox(0.0F, -2.0F, -2.0F, 3.0F, 12.0F, 4.0F, new CubeDeformation(0.0F)).mirror(false), PartPose.offsetAndRotation(4.0F, -22.0F, 0.0F, -0.9163F, 0.0F, 0.0F));
+        PartDefinition left_arm = torso.addOrReplaceChild("left_arm", CubeListBuilder.create().texOffs(14, 46).mirror().addBox(0.0F, -2.0F, -2.0F, 3.0F, 12.0F, 4.0F, new CubeDeformation(0.0F)).mirror(false), PartPose.offsetAndRotation(4.0F, -8.0F, 0.0F, -1.222F, 0.0149F, -0.041F));
 
-        PartDefinition right_arm = famished.addOrReplaceChild("right_arm", CubeListBuilder.create().texOffs(0, 46).addBox(-3.0F, -2.0F, -2.0F, 3.0F, 12.0F, 4.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(-4.0F, -22.0F, 0.0F, -0.9163F, 0.0F, 0.0F));
+        PartDefinition right_arm = torso.addOrReplaceChild("right_arm", CubeListBuilder.create().texOffs(0, 46).addBox(-3.0F, -2.0F, -2.0F, 3.0F, 12.0F, 4.0F, new CubeDeformation(0.0F)), PartPose.offsetAndRotation(-4.0F, -8.0F, 0.0F, -1.222F, -0.0149F, 0.041F));
 
-        PartDefinition left_leg = famished.addOrReplaceChild("left_leg", CubeListBuilder.create().texOffs(14, 28).addBox(-0.9F, 2.0F, -2.0F, 3.0F, 14.0F, 4.0F, new CubeDeformation(0.0F)), PartPose.offset(0.9F, -16.0F, 0.0F));
+        PartDefinition left_leg = famished.addOrReplaceChild("left_leg", CubeListBuilder.create().texOffs(14, 28).addBox(-0.9F, 0.0F, -2.0F, 3.0F, 14.0F, 4.0F, new CubeDeformation(0.0F)), PartPose.offset(0.9F, -14.0F, 0.0F));
 
-        PartDefinition right_leg = famished.addOrReplaceChild("right_leg", CubeListBuilder.create().texOffs(0, 28).addBox(-2.1F, 2.0F, -2.0F, 3.0F, 14.0F, 4.0F, new CubeDeformation(0.0F)), PartPose.offset(-0.9F, -16.0F, 0.0F));
+        PartDefinition right_leg = famished.addOrReplaceChild("right_leg", CubeListBuilder.create().texOffs(0, 28).addBox(-2.1F, 0.0F, -2.0F, 3.0F, 14.0F, 4.0F, new CubeDeformation(0.0F)), PartPose.offset(-0.9F, -14.0F, 0.0F));
 
         return LayerDefinition.create(meshdefinition, 64, 64);
     }
@@ -58,14 +54,10 @@ public class FamishedModel<T extends Famished> extends HierarchicalModel<T> {
             this.animateWalk(FamishedAnimations.ANIM_FAMISHED_AGGRO_WALK, limbSwing, limbSwingAmount, 1f, 54);
             this.animate(entity.idleAnimationState,FamishedAnimations.ANIM_FAMISHED_AGGRO_IDLE,ageInTicks, 1f);
             this.animate(entity.attackAnimationState,FamishedAnimations.ANIM_FAMISHED_AGGRO_ATTACK,ageInTicks, 1f);
-            head.visible = false;
-            head_hungry.visible = true;
         } else {
             this.animateWalk(FamishedAnimations.ANIM_FAMISHED_WALK, limbSwing, limbSwingAmount, 4f, 54);
             this.animate(entity.idleAnimationState,FamishedAnimations.ANIM_FAMISHED_IDLE,ageInTicks, 1f);
             this.animate(entity.attackAnimationState,FamishedAnimations.ANIM_FAMISHED_ATTACK,ageInTicks, 1f);
-            head.visible = true;
-            head_hungry.visible = false;
         }
     }
 
@@ -75,8 +67,6 @@ public class FamishedModel<T extends Famished> extends HierarchicalModel<T> {
 
         this.head.yRot = headYaw * ((float)Math.PI / 180f);
         this.head.xRot = headPitch *  ((float)Math.PI / 180f);
-        this.head_hungry.yRot = headYaw * ((float)Math.PI / 180f);
-        this.head_hungry.xRot = headPitch *  ((float)Math.PI / 180f);
     }
 
     @Override
