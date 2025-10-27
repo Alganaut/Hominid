b49a695bf2aa1e3bb4474c70e2d79ca9d602ed41
Alganaut <emretug624@gmail.com>
2025-10-27 19:33:23 +0000

    new juggernaut anims wip

--- src/main/java/com/alganaut/hominid/registry/entity/client/JuggernautModel.java
+++ src/main/java/com/alganaut/hominid/registry/entity/client/JuggernautModel.java
@@ -19,7 +19,7 @@ public class JuggernautModel<T extends Juggernaut> extends HierarchicalModel<T> 
     public JuggernautModel(ModelPart root) {
         this.juggernaut = root.getChild("juggernaut");
         this.torso = this.juggernaut.getChild("torso");
-        this.head = this.torso.getChild("head");
+        this.head = this.juggernaut.getChild("head");
     }
 
     public static LayerDefinition createBodyLayer() {
@@ -28,31 +28,22 @@ public class JuggernautModel<T extends Juggernaut> extends HierarchicalModel<T> 
 
         PartDefinition juggernaut = partdefinition.addOrReplaceChild("juggernaut", CubeListBuilder.create(), PartPose.offset(0.0F, 24.0F, 0.0F));
 
-        PartDefinition torso = juggernaut.addOrReplaceChild("torso", CubeListBuilder.create(), PartPose.offset(0.0F, -12.0F, 0.0F));
+        PartDefinition torso = juggernaut.addOrReplaceChild("torso", CubeListBuilder.create().texOffs(0, 0).addBox(-6.0F, -10.0F, -4.0F, 12.0F, 10.0F, 8.0F, new CubeDeformation(0.0F))
+                .texOffs(20, 47).addBox(-5.0F, 0.0F, -3.0F, 10.0F, 3.0F, 6.0F, new CubeDeformation(0.0F)), PartPose.offset(0.0F, -16.0F, 0.0F));
 
-        PartDefinition head = torso.addOrReplaceChild("head", CubeListBuilder.create().texOffs(0, 0).addBox(-4.0F, -8.0F, -4.0F, 8.0F, 8.0F, 8.0F, new CubeDeformation(0.0F))
-                .texOffs(32, 0).addBox(-4.0F, -8.0F, -4.0F, 8.0F, 8.0F, 8.0F, new CubeDeformation(0.5F)), PartPose.offset(0.0F, -12.0F, 0.0F));
+        PartDefinition left_leg = juggernaut.addOrReplaceChild("left_leg", CubeListBuilder.create().texOffs(0, 38).addBox(-2.0F, 0.0F, -3.0F, 5.0F, 13.0F, 5.0F, new CubeDeformation(0.0F)), PartPose.offset(2.0F, -13.0F, 0.5F));
 
-        PartDefinition body = torso.addOrReplaceChild("body", CubeListBuilder.create().texOffs(26, 16).addBox(-4.0F, 0.0F, -2.0F, 8.0F, 12.0F, 4.0F, new CubeDeformation(0.0F))
-                .texOffs(26, 32).addBox(-4.0F, 0.0F, -2.0F, 8.0F, 12.0F, 4.0F, new CubeDeformation(0.25F)), PartPose.offset(0.0F, -12.0F, 0.0F));
+        PartDefinition right_leg = juggernaut.addOrReplaceChild("right_leg", CubeListBuilder.create().texOffs(0, 38).mirror().addBox(-3.0F, 0.0F, -3.0F, 5.0F, 13.0F, 5.0F, new CubeDeformation(0.0F)).mirror(false), PartPose.offset(-2.0F, -13.0F, 0.5F));
 
-        PartDefinition chestplate_r1 = body.addOrReplaceChild("chestplate_r1", CubeListBuilder.create().texOffs(0, 32).addBox(-7.0F, -6.5F, -1.0F, 8.0F, 6.0F, 4.0F, new CubeDeformation(-0.001F)), PartPose.offsetAndRotation(3.0F, 7.0F, -1.0F, 0.5672F, 0.0F, 0.0F));
+        PartDefinition left_arm = juggernaut.addOrReplaceChild("left_arm", CubeListBuilder.create().texOffs(36, 27).addBox(0.0F, -2.0F, -3.0F, 5.0F, 14.0F, 5.0F, new CubeDeformation(0.0F))
+                .texOffs(42, 8).addBox(5.0F, -2.0F, -2.0F, 1.0F, 13.0F, 0.0F, new CubeDeformation(0.0F))
+                .texOffs(42, 8).addBox(5.0F, -2.0F, 1.0F, 1.0F, 13.0F, 0.0F, new CubeDeformation(0.0F)), PartPose.offset(6.0F, -24.0F, 0.5F));
 
-        PartDefinition left_arm = torso.addOrReplaceChild("left_arm", CubeListBuilder.create().texOffs(50, 16).mirror().addBox(-1.0F, -2.0F, -2.0F, 3.0F, 12.0F, 4.0F, new CubeDeformation(0.0F)).mirror(false)
-                .texOffs(40, 48).mirror().addBox(-1.0F, -1.9F, -2.0F, 3.0F, 12.0F, 4.0F, new CubeDeformation(0.26F)).mirror(false)
-                .texOffs(24, 48).mirror().addBox(-1.0F, -1.9F, -2.0F, 4.0F, 12.0F, 4.0F, new CubeDeformation(0.26F)).mirror(false)
-                .texOffs(54, 59).mirror().addBox(2.5F, 5.35F, -0.9F, 3.0F, 3.0F, 2.0F, new CubeDeformation(0.26F)).mirror(false), PartPose.offsetAndRotation(5.0F, -10.0F, 0.0F, -1.44F, 0.0057F, -0.0433F));
+        PartDefinition right_arm = juggernaut.addOrReplaceChild("right_arm", CubeListBuilder.create().texOffs(36, 27).mirror().addBox(-5.0F, -2.0F, -3.0F, 5.0F, 14.0F, 5.0F, new CubeDeformation(0.0F)).mirror(false)
+                .texOffs(42, 8).mirror().addBox(-6.0F, -2.0F, -2.0F, 1.0F, 13.0F, 0.0F, new CubeDeformation(0.0F)).mirror(false)
+                .texOffs(42, 8).mirror().addBox(-6.0F, -2.0F, 1.0F, 1.0F, 13.0F, 0.0F, new CubeDeformation(0.0F)).mirror(false), PartPose.offset(-6.0F, -24.0F, 0.5F));
 
-        PartDefinition right_arm = torso.addOrReplaceChild("right_arm", CubeListBuilder.create().texOffs(50, 16).addBox(-2.0F, -2.0F, -2.0F, 3.0F, 12.0F, 4.0F, new CubeDeformation(0.0F))
-                .texOffs(40, 48).addBox(-2.0F, -1.9F, -2.0F, 3.0F, 12.0F, 4.0F, new CubeDeformation(0.26F))
-                .texOffs(24, 48).addBox(-3.0F, -1.9F, -2.0F, 4.0F, 12.0F, 4.0F, new CubeDeformation(0.26F))
-                .texOffs(54, 59).addBox(-5.5F, 5.25F, -1.0F, 3.0F, 3.0F, 2.0F, new CubeDeformation(0.26F)), PartPose.offsetAndRotation(-5.0F, -10.0F, 0.0F, -1.44F, -0.0057F, 0.0433F));
-
-        PartDefinition left_leg = juggernaut.addOrReplaceChild("left_leg", CubeListBuilder.create().texOffs(10, 16).mirror().addBox(-1.901F, 0.0F, -2.0F, 4.0F, 12.0F, 4.0F, new CubeDeformation(0.0F)).mirror(false)
-                .texOffs(0, 42).mirror().addBox(-1.901F, 0.0F, -2.0F, 4.0F, 12.0F, 4.0F, new CubeDeformation(0.25F)).mirror(false), PartPose.offset(1.9F, -12.0F, 0.0F));
-
-        PartDefinition right_leg = juggernaut.addOrReplaceChild("right_leg", CubeListBuilder.create().texOffs(10, 16).addBox(-2.101F, 0.0F, -2.0F, 4.0F, 12.0F, 4.0F, new CubeDeformation(0.0F))
-                .texOffs(0, 42).addBox(-2.101F, 0.0F, -2.0F, 4.0F, 12.0F, 4.0F, new CubeDeformation(0.25F)), PartPose.offset(-1.9F, -12.0F, 0.0F));
+        PartDefinition head = juggernaut.addOrReplaceChild("head", CubeListBuilder.create().texOffs(0, 18).addBox(-5.0F, -12.0F, -4.0F, 10.0F, 12.0F, 8.0F, new CubeDeformation(0.0F)), PartPose.offset(0.0F, -26.0F, 0.0F));
 
         return LayerDefinition.create(meshdefinition, 64, 64);
     }
