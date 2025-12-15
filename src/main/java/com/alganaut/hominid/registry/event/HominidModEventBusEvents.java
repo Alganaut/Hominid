8fa013d517a81ba4cea7ed8e7b5dab8494274e2f
Alganaut <emretug624@gmail.com>
2025-12-15 19:35:32 +0000

    super fixed pack

--- src/main/java/com/alganaut/hominid/registry/event/HominidModEventBusEvents.java
+++ src/main/java/com/alganaut/hominid/registry/event/HominidModEventBusEvents.java
@@ -55,6 +55,7 @@ public class HominidModEventBusEvents {
     public static void addBuiltinPacks(AddPackFindersEvent event) {
         if (event.getPackType() == PackType.CLIENT_RESOURCES) { // Resource Packs
             HominidBuiltinPacks.rpHominidRetextures(event);
+            HominidBuiltinPacks.rpHominidModdedRetextures(event);
         }
         if (event.getPackType() == PackType.SERVER_DATA) { // Datapacks
 
--- src/main/java/com/alganaut/hominid/registry/misc/HominidBuiltinPacks.java
+++ src/main/java/com/alganaut/hominid/registry/misc/HominidBuiltinPacks.java
@@ -34,7 +34,7 @@ public class HominidBuiltinPacks {
 
     public static void rpHominidModdedRetextures(AddPackFindersEvent event) {
         Path path = ModList.get().getModFileById(Hominid.MODID).getFile().findResource("resourcepacks/hominid_modded_retextures");
-        PackMetadataSection metadata = new PackMetadataSection(Component.literal("Changes other modded mobs to fit Hominid's style."), SharedConstants.getCurrentVersion().getPackVersion(PackType.CLIENT_RESOURCES));
+        PackMetadataSection metadata = new PackMetadataSection(Component.literal("Changes other modded mobs to fit Hominid's style"), SharedConstants.getCurrentVersion().getPackVersion(PackType.CLIENT_RESOURCES));
         event.addRepositorySource(source -> source.accept(new Pack(
                 new PackLocationInfo("hominid:hominid_modded_retextures", Component.literal("Hominid Modded Retextures"), PackSource.BUILT_IN, Optional.empty()),
                 new PathPackResources.PathResourcesSupplier(path),
