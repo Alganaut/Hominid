6b5c61e126bcca3ac03b18adad0c3396dfb5d9c8
Alganaut <emretug624@gmail.com>
2026-05-03 09:43:05 +0100

    resource pack merge and vanilla tags

--- src/main/java/com/alganaut/hominid/registry/misc/HominidBuiltinPacks.java
+++ src/main/java/com/alganaut/hominid/registry/misc/HominidBuiltinPacks.java
@@ -23,20 +23,9 @@ public class HominidBuiltinPacks {
 
     public static void rpHominidRetextures(AddPackFindersEvent event) {
         Path path = ModList.get().getModFileById(Hominid.MODID).getFile().findResource("resourcepacks/hominid_retextures");
-        PackMetadataSection metadata = new PackMetadataSection(Component.literal("Allows vanilla mobs to work with Hominid correctly"), SharedConstants.getCurrentVersion().getPackVersion(PackType.CLIENT_RESOURCES));
+        PackMetadataSection metadata = new PackMetadataSection(Component.literal("Changes vanilla and modded mobs to work with Hominid"), SharedConstants.getCurrentVersion().getPackVersion(PackType.CLIENT_RESOURCES));
         event.addRepositorySource(source -> source.accept(new Pack(
                 new PackLocationInfo("hominid:hominid_retextures", Component.literal("Hominid Retextures"), PackSource.BUILT_IN, Optional.empty()),
-                new PathPackResources.PathResourcesSupplier(path),
-                new Pack.Metadata(metadata.description(), PackCompatibility.COMPATIBLE, FeatureFlagSet.of(), List.of(), false),
-                new PackSelectionConfig(false, Pack.Position.TOP, false)
-        )));
-    }
-
-    public static void rpHominidModdedRetextures(AddPackFindersEvent event) {
-        Path path = ModList.get().getModFileById(Hominid.MODID).getFile().findResource("resourcepacks/hominid_modded_retextures");
-        PackMetadataSection metadata = new PackMetadataSection(Component.literal("Changes other modded mobs to fit Hominid's style"), SharedConstants.getCurrentVersion().getPackVersion(PackType.CLIENT_RESOURCES));
-        event.addRepositorySource(source -> source.accept(new Pack(
-                new PackLocationInfo("hominid:hominid_modded_retextures", Component.literal("Hominid Modded Retextures"), PackSource.BUILT_IN, Optional.empty()),
                 new PathPackResources.PathResourcesSupplier(path),
                 new Pack.Metadata(metadata.description(), PackCompatibility.COMPATIBLE, FeatureFlagSet.of(), List.of(), false),
                 new PackSelectionConfig(false, Pack.Position.TOP, false)
