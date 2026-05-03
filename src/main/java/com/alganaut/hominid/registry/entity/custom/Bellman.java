7fd9ae79fdcab91efd8d43dafa50acacd341422e
Alganaut <emretug624@gmail.com>
2026-05-03 10:26:33 +0100

    spawn and dd bellman compat

--- src/main/java/com/alganaut/hominid/registry/entity/custom/Bellman.java
+++ src/main/java/com/alganaut/hominid/registry/entity/custom/Bellman.java
@@ -178,7 +178,9 @@ public class Bellman extends Monster {
                             BuiltInRegistries.ENTITY_TYPE.get(ResourceLocation.fromNamespaceAndPath("alexscaves", "boundroid")),
                             BuiltInRegistries.ENTITY_TYPE.get(ResourceLocation.fromNamespaceAndPath("alexscaves", "caniac")),
                             BuiltInRegistries.ENTITY_TYPE.get(ResourceLocation.fromNamespaceAndPath("alexscaves", "brainiac")),
-                            BuiltInRegistries.ENTITY_TYPE.get(ResourceLocation.fromNamespaceAndPath("netherexp", "vessel"))
+                            BuiltInRegistries.ENTITY_TYPE.get(ResourceLocation.fromNamespaceAndPath("netherexp", "vessel")),
+                            BuiltInRegistries.ENTITY_TYPE.get(ResourceLocation.fromNamespaceAndPath("dungeonsdelight", "zombified_dryad")),
+                            BuiltInRegistries.ENTITY_TYPE.get(ResourceLocation.fromNamespaceAndPath("spawn", "barbed"))
                     };
                 }
 
